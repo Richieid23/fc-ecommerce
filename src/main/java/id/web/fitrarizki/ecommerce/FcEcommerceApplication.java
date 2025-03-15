50770afa96d264054c22e31902a9d856fc50d966
Richi <fitrarizki23@gmail.com>
2025-03-15 12:53:24 +0700

    init commit

---
+++ src/main/java/id/web/fitrarizki/ecommerce/FcEcommerceApplication.java
@@ -0,0 +1,13 @@
+package id.web.fitrarizki.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/web/fitrarizki/ecommerce/FcEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.web.fitrarizki.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
